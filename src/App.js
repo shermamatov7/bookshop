097752f4b6d050620cb741f399b226cbf98788f5
bayastanchik7 <bayastantursumamatov@gmail.com>
2024-03-09 12:47:33 +0600

    header

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
       <h1>Bayastan</h1>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum libero enim error ipsam, commodi pariatur est saepe voluptas repellat exercitationem, incidunt hic totam tenetur veniam natus rem soluta eveniet necessitatibus.</p>
       <button>rfdh</button>
+      {/* bayastan */}
     </div>
   );
 }
