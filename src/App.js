ed5841714895c0237c18be83bd25ad56de6f327d
shermamatov7 <158985512+shermamatov7@users.noreply.github.com>
2024-03-10 12:20:29 +0600

    book (#4)
    Co-authored-by: bayastanchik7 <bayastantursumamatov@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import './App.css';
 import Header from './components/Header/Header';
+import Hero from './pages/Hero/Hero';
+import Categori from './pages/Categori/Categori';
+import Line from './pages/Line/line.jsx'
 
 
 function App() {
   return (
     <div className="App">
-     <Header/>
+      <Header />
+      <Hero />
+      <Categori />
+      <Line />
     </div>
   );
 }
